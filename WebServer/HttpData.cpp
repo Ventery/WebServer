ca969422ccb716a31ce0b57414bbe265fb26f365
Ventery <HP@huangmenjimifanweila.local>
2021-08-14 11:16:33 +0800

    v1.0

--- WebServer/HttpData.cpp
+++ WebServer/HttpData.cpp
@@ -818,7 +818,7 @@ AnalysisState HttpData::analysisRequest()
         {
           stat((application_path_prefix + file_lists[num]).c_str(), &tp_sbuf);
           if (tp_sbuf.st_size<2*1024)
-            TITLE+="<object width="1000" data=\""+std::string("/source.txt?") + application_path_prefix + file_lists[num]+"\"></object>";
+            TITLE+=string("<object width="1000" data=\"")+std::string("/source.txt?") + application_path_prefix + file_lists[num]+"\"></object>";
           else TITLE+="文件过大无法预览";
         }
       }
