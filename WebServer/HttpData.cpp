5cad4021fc3eefbe9129638bdb309aec9b3f2df3
Ventery <HP@huangmenjimifanweila.local>
2021-08-18 20:21:50 +0800

    add file transmission

--- WebServer/HttpData.cpp
+++ WebServer/HttpData.cpp
@@ -689,6 +689,7 @@ HeaderState HttpData::parseHeaders() {
   }
   cout<<hState_<<" "<<i<<" "<<str.c_str()[i-1] <<str.c_str()[i]<<endl;
   cout<<(int)str.c_str()[i-1]<<" "<<(int)str.c_str()[i]<<endl;
+  cout<<(int)str.c_str()[0]<<endl;
   //cout<<str<<endl;
   if (hState_ == H_END_LF) {
     str = str.substr(i);
